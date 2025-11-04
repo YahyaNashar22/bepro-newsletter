153f36c1ad6e99b3c1f165281ddee9e51be64ee9
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-11-04 07:31:59 +0200

    added the extra size for uploads

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(cors({
 ));
 
 // Configuration Middlewares
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("uploads"));
 
 // routes
